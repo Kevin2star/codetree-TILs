74b1939a629206a016f959620286a607125990ce
Kevin2star <165244900+Kevin2star@users.noreply.github.com>
2024-07-10 16:48:19 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240710/시력 검사 2/eye-test-2.c
+++ 240710/시력 검사 2/eye-test-2.c
@@ -4,9 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     double a;
     scanf("%lf", &a);
-    if(a>1.0)
+    if(a>=1.0)
         printf("High");
-    else if(a>0.5)
+    else if(a>=0.5)
         printf("Middle");
     else
         printf("Low");

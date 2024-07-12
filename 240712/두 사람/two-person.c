7c2c500413136c4716ef6f35292f131f7ce3d5aa
Kevin2star <165244900+Kevin2star@users.noreply.github.com>
2024-07-12 13:55:36 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240712/두 사람/two-person.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int fa, sa;
+    char fs, ss;
+    scanf("%d %c %d %c", &fa, &fs, &sa, &ss);
+    if((fa>=19 && fs=='M') || (sa>=19 && sa=='M'))
+        printf("1");
+    else
+        printf("0");
+    return 0;
+}

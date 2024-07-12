1f7599abf39b4f5217b0e7b5a466004931ed6fdd
Kevin2star <165244900+Kevin2star@users.noreply.github.com>
2024-07-12 13:57:01 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/두 사람/two-person.c
+++ 240712/두 사람/two-person.c
@@ -5,7 +5,7 @@ int main() {
     int fa, sa;
     char fs, ss;
     scanf("%d %c %d %c", &fa, &fs, &sa, &ss);
-    if((fa>=19 && fs=='M') || (sa>=19 && sa=='M'))
+    if((fa>=19 && fs=='M') || (sa>=19 && ss=='M'))
         printf("1");
     else
         printf("0");
